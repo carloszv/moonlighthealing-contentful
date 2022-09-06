4d723d8b3252fb149910f359e146ee6d4ed1a212
Carlos Zamora <carlos.zamora@bob.ch>
2022-09-06 15:09:49 +0200

    renaming files

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import styled from 'styled-components'
 import { PROJECT_NAME } from '../lib/constants'
 import { HeaderProps } from '../types/Header'
-import { AppSidebar } from './AppSideBars'
+import { AppSidebar } from './AppSideBar'
 import { ContentfulImage } from './ContentfulImage'
 
 export const AppHeader = (props: HeaderProps) => {
